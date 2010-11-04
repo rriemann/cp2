d777bd34599c46da8f877ce94e7c873eb469db4d
Thomas Murach <murach@physik.hu-berlin.de>
2010-11-04 20:49:16 +0100

    es geht

--- 01/eig_custom.m
+++ 01/eig_custom.m
@@ -7,29 +7,35 @@ function res = eig_custom(A)
 
   max_iterations = 1e5;
   A = (A + A.')/2;
+  Abak = A;
 
   n = size(A)(1);
   A1 = A;
+  V = eye(n);
   # A1 = zeros(n);
   iterations = 0;
 
   % berechne S(A):
   A2 = A.^2;
-  ss = sum(sum(A2))-trace(A2); % ineffizient, da Spur-Elemente doppelt berechnet werden
+  ssum = sum(sum(A2))-trace(A2); % ineffizient, da Spur-Elemente doppelt berechnet werden
 
-  while iterations < max_iterations
+  #while iterations < max_iterations
+  while abs(ssum) > 10*eps*(n^2-n)
     #fuer aufgabe b)
-    V = eye(n);
+    #V = eye(n);
     counter = 0;
     
-    if ss > 10*eps*(n^2-n)
+    #if ss > 10*eps*(n^2-n)
       for q = [2:n]
         for p = [1:q-1]
-          if A(p,q) ~= 0
+          #if A(p,q) ~= 0
             theta = (A(q,q)-A(p,p))/(2*A(p,q));
             t = sign(theta)/(abs(theta)+sqrt(theta^2+1));
             c = 1/sqrt(t^2+1);
             s = t*c;
+            tau = s/(1+c);
+            %ssum = ssum - 2*abs(A(p,q)^2)
+            %A1 = A;
             
             if counter == 0
               V(p,p) = c;
@@ -39,13 +45,11 @@ function res = eig_custom(A)
             end
             counter = 1;
             
-            tau = s/(1+c);
-            #A1
             for i = [[1:p-1], [p+1:q-1], [q+1:n]]
               A1(i,p) = A(i,p)-s*(A(i,q)+tau*A(i,p));
               A1(p,i) = A1(i,p);
               A1(i,q) = A(i,q)+s*(A(i,p)-tau*A(i,q));
-              A1(q,i) = A(i,q);
+              A1(q,i) = A1(i,q);
               if counter > 0
                 V(i,p) = V(p,p)*c - V(q,p)*s;
                 V(i,q) = V(p,q)*c + V(q,q)*s;
@@ -56,22 +60,21 @@ function res = eig_custom(A)
             A1(q,q) = A(q,q)+t*A(p,q);
             A1(p,q) = 0;
             A1(q,p) = 0;
-            #[q,p]
-            #A
-            #A1
-            #pause;
-          end
+            #A = A1;
+            ssum = ssum - 2*abs(A(p,q)^2)
+            A = A1
+          #end
         end
       end
-      ss = ss - 2*A(p,q)^2;
-      A = A1;
+      #ssum = ssum - 2*A(p,q)^2
+      #A = A1;
       iterations = iterations + 1;
-    else
-      break;
-    end
+    #else
+    #  break;
+    #end
   end
   disp('Iterationen:');
   disp(iterations);
-  A*A'
+  #V'*Abak*V
   res = A;
 end
