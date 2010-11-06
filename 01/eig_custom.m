81a73ea845f138becbb7f3f1f885a9112c45b9c1
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2010-11-06 11:36:50 +0100

    flops

--- 01/eig_custom.m
+++ 01/eig_custom.m
@@ -1,59 +1,59 @@
-%!/usr/bin/octave -q
+#!/usr/bin/octave -q
 % kate: remove-trailing-space on; replace-trailing-space-save on; indent-width 2; indent-mode normal; syntax matlab; space-indent on;
 
 function res = eig_custom(A)
-  % berechnet zu einer reellen, symmetrischen Matrix (A = A^T) die Eigenwerte
-  % und gibt sie in einer Diagonalmatrix wieder zurück.
+  % [λ₁, λ₂, …] = eig_custom(A)
+  % berechnet zur (n×n)-Matrix (A+A')/2 die Eigenwerte
+  % und gibt sie als sortieren Spaltenvektor aus
 
-  max_iterations = 1e5;
   A = (A + A.')/2;
   Abak = A;
 
   n = size(A)(1);
   A1 = A;
-  V = eye(n);
-  V1 = V;
+  V = V1 = eye(n);
   iterations = 0;
 
   % berechne S(A):
   A2 = A.^2;
-  ssum = sum(sum(A2))-trace(A2); % ineffizient, da Spur-Elemente doppelt berechnet werden
+  ssum = sum(sum(A2))-trace(A2); % ineffizient, da 2mal Spur-Elemente-Berechnung
 
-  while abs(ssum) > eps*(n^2-n)
+  while ssum > eps*(n^2-n)
+    for q = [2:n] % flops: •n
+      for p = [1:q-1] % flops: •n
+          theta = (A(q,q)-A(p,p))/(2*A(p,q)); % flops: 2
+          t = sign(theta)/(abs(theta)+sqrt(theta^2+1)); % flops: 2
+          c = 1/sqrt(t^2+1); % flops: 2
+          s = t*c; % flops: 1
+          tau = s/(1+c); % flops: 1
 
-      for q = [2:n]
-        for p = [1:q-1]
-            theta = (A(q,q)-A(p,p))/(2*A(p,q));
-            t = sign(theta)/(abs(theta)+sqrt(theta^2+1));
-            c = 1/sqrt(t^2+1);
-            s = t*c;
-            tau = s/(1+c);
+          % Berechnung der Eigenvektoren
+          V1(:,p) += -s*(V(:,q)+tau*V(:,p));
+          V1(:,q) += +s*(V(:,p)-tau*V(:,q));
+          V = V1;
 
-            for i = [[1:p-1], [p+1:q-1], [q+1:n]]
-              A1(i,p) = A(i,p)-s*(A(i,q)+tau*A(i,p));
-              A1(p,i) = A1(i,p);
-              A1(i,q) = A(i,q)+s*(A(i,p)-tau*A(i,q));
-              A1(q,i) = A1(i,q);
-            end
+          % Diagonalisierung nach Jacobi-Methode
+          i = [[1:p-1], [p+1:q-1], [q+1:n]]; % flops: •n
+          A1(i,p) = A1(p,i) = A(i,p)-s*(A(i,q)+tau*A(i,p)); % flops: 2
+          A1(i,q) = A1(q,i) = A(i,q)+s*(A(i,p)-tau*A(i,q)); % flops: 2
 
-            V1(:,p) = V(:,p) - s*(V(:,q) + tau*V(:,p));
-            V1(:,q) = V(:,q) + s*(V(:,p) - tau*V(:,q));
-            V = V1;
+          A1(p,p) = A(p,p)-t*A(p,q); % flops: 1
+          A1(q,q) = A(q,q)+t*A(p,q); % flops: 1
+          A1(p,q) = A1(q,p) = 0;
 
-            A1(p,p) = A(p,p)-t*A(p,q);
-            A1(q,q) = A(q,q)+t*A(p,q);
-            A1(p,q) = 0;
-            A1(q,p) = 0;
-            ssum = ssum - 2*abs(A(p,q)^2);
-            A = A1;
-        end
+          ssum -= 2*abs(A(p,q)^2); % flops: 2
+          A = A1;
       end
-      iterations = iterations + 1;
+    end
+    iterations++;
   end
   disp('Iterationen:');
   disp(iterations);
-  V.'*Abak*V
-  V'*V
+  disp('V''•A•V:');
+  disp(V.'*Abak*V);
+  disp('V''•V:');
+  disp(V'*V);
   res = sort(diag(A));
-  res-eig(Abak)
+  disp('res-eig(A):');
+  disp(res-eig(Abak));
 end
