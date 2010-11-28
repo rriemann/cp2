80627cd04b0bd770162a41cd30776d83b26f41da
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2010-11-28 16:38:42 +0100

    symmetrisiert

--- 04/code/fourier.m
+++ 04/code/fourier.m
@@ -1,3 +1,4 @@
+% kate: remove-trailing-space on; replace-trailing-space-save on; indent-width 2; indent-mode normal; syntax matlab; space-indent on;
 %  4.1
 disp("Vektor eins: ");
 v1 = [1;2;3;4;5]
@@ -16,12 +17,12 @@ ruecktrafo(f2,1)
 
 T = 10;
 N = 101;
-w0 = 2*pi/T;
+w0 = 5*2*pi/T;
 
 sinfkt = sin(w0*[0:T/(N-1):T]');
 %  w = trafo([0:T/(N-1):T]',T);		% berechnung der omegas. korrekt?
 % w entspricht k in der umgesetzten formel. daher ist w = 2pi/T*n mit n = [0:100]
-w = 2*pi/T*[0:100]';
+w = 2*pi/T*[[0:50],[-50:-1]]';
 f = trafo(sinfkt,1);
 plot(w,(abs(f)).^2);			% ka, ob das zu erwarten war ;)
 print("sintrafo.png");
