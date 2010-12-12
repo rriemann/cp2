567621587f03bb2aa5f154c1d86dfc96e58c61f3
Thomas Murach <murach@physik.hu-berlin.de>
2010-12-12 17:17:53 +0100

    bis d

--- 06/code/ho_fou.m
+++ 06/code/ho_fou.m
@@ -1,9 +1,4 @@
-%  Erzeugung Diskretisierung und 
-%  Hamilton Operator fuer QM Harmonischen Oszillator
-%  mit Fourier Ableitung
-%  Aufruf [a,x,H] = ho_fou(L,N)
-%
-function [a,x,p,H] = ho_fou(L,N)
+function [a,x,p] = ho_fou(L,N)
 
 if ~mod(N,2), error('ho_fou.m: N muss ungerade sein');end
 M=(N-1)/2;
@@ -16,18 +11,13 @@ p = zeros(N);
 %  kinetischer Teil des Hamilton:
 A = zeros(N,N);
 for i=1:N, A(i,:) = x(i) - x; end               % Matrix (x_i - x_j)
-H = zeros(N,N);
 for i=1:N,
-  H      = H + imp(i)^2*cos(imp(i)*A);
-  for j=abs(imp),
+  for j=imp(N/2+0.5:N),
     p(i,:) += 2*sqrt(-1)/N*j*sin(j*(x(i).-x));
   end
 end      % kein i*sin wg. p<->-p Symm. 
-H = H*(0.5/N);                                  % (-1/2) Laplace
 
 %  Potential, Oszillator Teil des Hamilton:
-H=H+0.5*diag(x.^2);
 x = diag(x);
 
-disp(' H.O. Hamilton mit Fourier-diskretisiertem Laplace Operator: ')
 fprintf(' Raumintervall [%g , %g] mit %i Punkten \n',[-L/2 L/2 N]);
--- 06/code/steuerung.m
+++ 06/code/steuerung.m
@@ -1,8 +1,8 @@
-%  aufgabe 6.1a)
-[a,x,p,H] = ho_fou(10,11);
+%  aufgabe 6.1b)
+[a,x,p] = ho_fou(10,11);
 
 
-%  6.2a)
+%  6.1c)
 C = i*(p*x-x*p);
 
 figure(1);
@@ -13,3 +13,26 @@ mesh(real(x),real(C));
 print("../tmp/mesh_c_over_x.pdf");
 mesh(real(C));
 print("../tmp/mesh_c.pdf");
+
+%  6.1d
+L = 20;
+N = 101;
+[a,x,p] = ho_fou(L,N);
+C = i*(p*x-x*p);
+x = sum(x);		% zeilenvektor
+Diff = zeros(L);
+sigma = 0.5;		% beliebig
+
+for z1 = 1:L+1,
+  psi1 = exp(-1/2*(x-(z1-L/2-1)).^2/sigma^2);
+  psi1 = psi1/sqrt(psi1*psi1');
+  for z2 = 1:L+1,
+    psi2 = exp(-1/2*(x-(z2-L/2-1)).^2/sigma^2);
+    psi2 = psi2/sqrt(psi2*psi2');
+    Diff(z1,z2) = psi1*C*psi2'-psi1*psi2';
+  end
+end
+
+mesh(Diff);
+axis([0,21,0,21,-2,1]);
+print("../tmp/Diff.pdf");
