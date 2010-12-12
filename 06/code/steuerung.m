67dd9eb2354574c1deba71cdfb030952e37cd040
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2010-12-12 18:38:54 +0100

    6.2a

--- 06/code/steuerung.m
+++ 06/code/steuerung.m
@@ -1,8 +1,8 @@
-%  aufgabe 6.1b)
+%  aufgabe 6.1a)
 [a,x,p] = ho_fou(10,11);
 
 
-%  6.1c)
+%  6.2a)
 C = i*(p*x-x*p);
 
 figure(1);
@@ -14,27 +14,35 @@ print("../tmp/mesh_c_over_x.pdf");
 mesh(real(C));
 print("../tmp/mesh_c.pdf");
 
-%  6.1d
-L = 20;
-N = 101;
-[a,x,p] = ho_fou(L,N);
-C = i*(p*x-x*p);
-x = sum(x);		% zeilenvektor
-Diff = zeros(L);
-sigma = 0.5;		% beliebig
+% 6.2a
 
-for z1 = 1:L+1,
-  psi1 = exp(-1/2*(x-(z1-L/2-1)).^2/sigma^2);
-  psi1 = psi1/sqrt(psi1*psi1');
-  for z2 = 1:L+1,
-    psi2 = exp(-1/2*(x-(z2-L/2-1)).^2/sigma^2);
-    psi2 = psi2/sqrt(psi2*psi2');
-    Diff(z1,z2) = psi1*C*psi2'-psi1*psi2';
+function [H] = homo_osc(alpha = 2, L = 20, N = 151)
+  if ~mod(N,2)
+    error('N muss ungerade sein')
   end
+  M=(N-1)/2;
+  a = L/N;
+  x=a*(-M:M);
+  p=(2*pi/L)*(-M:M);
+
+  %  kinetischer Teil des Hamilton:
+  A = H = zeros(N,N);
+  for i=[1:N]
+    A(i,:) = x(i) - x; % Matrix (x_i - x_j)
+  end
+
+  for i=[1:N]
+    H += p(i)^2*cos(p(i)*A); % kein i*sin wg. p<->-p Symm.
+  end
+  H = H*(0.5/N); % (-1/2) Laplace
+
+  %  Potential, Oszillator Teil des Hamilton:
+  H += 0.5*diag(abs(x).^alpha);
 end
 
-mean(mean(Diff))
+eigen_values = zeros(4,3);
+for alpha = [1:4]
+  eigen_values(alpha,:) = eig(homo_osc(alpha))(end-2:end)';
+end
 
-mesh(Diff);
-axis([0,21,0,21,-2,1]);
-print("../tmp/Diff.pdf");
+% kate: remove-trailing-space on; replace-trailing-space-save on; indent-width 2; indent-mode normal; syntax matlab; space-indent on;
