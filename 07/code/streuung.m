ab49c22cd6f11990d6ae01ec07c4119ee703e5c0
Thomas Murach <murach@physik.hu-berlin.de>
2011-01-01 23:37:01 +0100

    vl-prog laeuft

---
+++ 07/code/streuung.m
@@ -0,0 +1,41 @@
+clear all; hold off; clf;
+L=20; N=101;
+
+[a,x,H] = ho_fou(L,N);
+
+j = sqrt(-1);
+T0 = 2*pi;
+T = T0/10;
+U = expm(-j*T*H);
+
+x_z=3;
+sigma=1/sqrt(3);
+
+psi = exp(-(x-x_z).^2/(2*sigma^2)).';
+psi = psi/norm(psi);
+gauss=psi;
+p = psi.*conj(psi);	% Wahrscheinlichkeit
+
+plot(x,p,'-'); hold;
+
+t=0;
+for i=1:5
+  psi = U*psi; t=t+T;
+  p = psi.*conj(psi);
+  plot(x,p,'-');
+  pause(1);
+end
+
+xlabel('x'); ylabel('p');
+disp('PAUSE'); pause;
+hold off;clf;
+
+% Zeitentwicklung des x-Mittelwertes
+for i=1:100
+  xmean(i) = real((psi'.*x)*psi); % psi-kreuz x psi; sowieso reell
+  tplot(i)=t;
+  psi=U*psi; t=t+T;
+end
+
+plot(tplot/T0,xmean); hold; plot(tplot/T0,xmean,'x')
+xlabel('t/T0'); ylabel('<x>');
