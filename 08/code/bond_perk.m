153da4a48017fbb7225ed457c6cd5ddf1e624fe4
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2011-01-10 00:54:12 +0100

    bond_perk feld fertig

--- 08/code/bond_perk.m
+++ 08/code/bond_perk.m
@@ -1,20 +1,29 @@
-L=12;  % Gittergroesse L*L
-p=0.5;  % Besetzungswahrscheinlichkeit
+#!/usr/bin/env octave
+% kate: remove-trailing-space on; replace-trailing-space-save on; indent-width 2; indent-mode normal; syntax matlab; space-indent on;
+L=10;  % Gittergroesse L*L
+p=0.4;  % Besetzungswahrscheinlichkeit
 rand('state',0); % Standard initialisierung der Zufallszahlen
 
-disp('Punktbesetzung:');
-feld=-(rand(L,L) > p);
+disp('Bondbesetzung:');
+%  nach oben und rechts: wert = -3, wenn nur rechts: wert = -2, nur oben: -1; passiv: 0
+feld_rechts = (rand(L,L-1) > p); % 1 = verbindung nach rechts
+feld_oben = (rand(L-1,L) > p); % 1 = verbindung nach links
+feld = zeros(L,L);
+for i=1:L-1
+  feld(i,:) -= 2*feld_rechts;
+  feld(:,i) -= feld_oben;
+end
 flipud(feld.')  % druck-plot gegen (x,y)
-% Bedeutung feld-Werte: -1=leer, 0=besetzt, andere: 
+% Bedeutung feld-Werte: -1=leer, 0=besetzt, andere:
 % Cluster Indizes
 
 % Plot
 figure();axis([0 L+1 0 L+1]);hold on;
 % besetzt/leer:
-for x=1:L, for y=1:L
-   if feld(x,y) >= 0, plot(x,y,'*r'); else, plot(x,y,'.b'); end        
-end,end
-% Linien zwischen besetzten Nachbarn
+for y=1:L
+  plot(1:L,y,'*r');
+end
+% Linien
 for x=1:L-1, for y=1:L
     if feld(x,y) >= 0 & feld(x+1,y) >= 0, plot([x x+1],[y y],'-r'); end
 end,end
@@ -29,13 +38,12 @@ disp('Ergebnis, Baumsuche:');
 [feld] = baum_analyse(feld);
 flipud(feld.')
 disp('Ergebnis, Hoshen-Kopelman:');
-[feld] = hoshen_kopelman(feld); 
+[feld] = hoshen_kopelman(feld);
 flipud(feld.')
 
 %  bondperkolation - alle nachbarn immer verbunden, aber nur
 %  einige der verbindungen sind aktiv
 
-%  wenn nur horiz. aktive verbindung: wert = -2, nur vertikale verbindung: -1; passiv: 0
 %  dann immer fallunterscheidung zwischen diesen faellen
 %  die matrix hat demnach auch nur (Lx-1)x(Ly-1) als dimension
 %  von jedem punkt aus werden nur die rechten bzw. unteren nachbarn ueberprueft
