5eb1d9161046b357270c9ec9629616793889d009
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2011-01-10 12:44:20 +0100

    baumanalyse geht

--- 08/code/baum_analyse.m
+++ 08/code/baum_analyse.m
@@ -22,7 +22,7 @@ for ya = 1:Ly
         y = bond_liste(i,2);
         % rechten Nachbarn pruefen (Matrix-System)
         if y < Ly
-          if bond_feld(x,y) < -1
+          if bond_feld(x,y) < -1 && cluster_feld(x,y+1) <= 0
             cluster_feld(x,y+1) = cluster;
             n=n+1;
             bond_liste(n,:) = [x y+1];
@@ -30,7 +30,7 @@ for ya = 1:Ly
         end
         % linken Nachbarn pruefen (Matrix-System)
         if y > 1
-          if bond_feld(x,y-1) < -1
+          if bond_feld(x,y-1) < -1 && cluster_feld(x,y-1) <= 0
             cluster_feld(x,y-1) = cluster;
             n=n+1;
             bond_liste(n,:)=[x y-1];
@@ -38,7 +38,7 @@ for ya = 1:Ly
         end
         % oberen Nachbarn pruefen (Matrix-System)
         if x > 1
-          if rem(bond_feld(x-1,y),2) == -1
+          if rem(bond_feld(x-1,y),2) == -1 && cluster_feld(x-1,y) <= 0
             cluster_feld(x-1,y) = cluster;
             n=n+1;
             bond_liste(n,:)=[x-1 y];
@@ -46,7 +46,7 @@ for ya = 1:Ly
         end
         % unteren Nachbarn pruefen (Matrix-System)
         if x < Lx
-          if rem(bond_feld(x,y),2) == -1
+          if rem(bond_feld(x,y),2) == -1 && cluster_feld(x+1,y) <= 0
             cluster_feld(x+1,y) = cluster;
             n=n+1;
             bond_liste(n,:)=[x+1 y];
@@ -56,55 +56,3 @@ for ya = 1:Ly
     end
   end
 end
-
-%  liste=zeros(Lx*Ly,2); % Arbeitsliste fuer Clustersuche
-%  cluster=0;
-%
-%  for ya=1:Ly
-%  for xa=1:Lx
-%     if feldr(xa,ya)==0
-%         cluster=cluster+1; % neuer Index vergeben
-%         feldr(xa,ya)=cluster;
-%         liste(1,:)=[xa ya];
-%         n=1; % letzter benutzter Listenplatz
-%         i=0;
-%         while i < n
-%             i=i+1;
-%             x=liste(i,1);y=liste(i,2);
-%             % rechten Nachbarn pruefen
-%             if x < Lx,
-%                 if feldr(x+1,y)==0
-%                     feldr(x+1,y)=cluster;
-%                     n=n+1;
-%                     liste(n,:)=[x+1 y];
-%                 end
-%             end
-%             % linken Nachbarn pruefen
-%             if x > 1
-%                 if feldr(x-1,y)==0
-%                     feldr(x-1,y)=cluster;
-%                     n=n+1;
-%                     liste(n,:)=[x-1 y];
-%                 end
-%             end
-%             % oberen Nachbarn pruefen
-%             if y < Ly
-%                 if feldr(x,y+1)==0
-%                     feldr(x,y+1)=cluster;
-%                     n=n+1;
-%                     liste(n,:)=[x y+1];
-%                 end
-%             end
-%             % unteren Nachbarn pruefen
-%             if y > 1
-%                 if feldr(x,y-1)==0
-%                     feldr(x,y-1)=cluster;
-%                     n=n+1;
-%                     liste(n,:)=[x y-1];
-%                 end
-%             end
-%         end % while loop: Cluster komplett
-%  %       fprintf('\n Cluster % i aus %i  Punkten \n',cluster,n)
-%     end
-%  end
-%  end
