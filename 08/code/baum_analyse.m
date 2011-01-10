1f3ecf74fd50a78840746b00262f718b8de5e261
Thomas Murach <murach@physik.hu-berlin.de>
2011-01-10 03:03:14 +0100

    geht fast

--- 08/code/baum_analyse.m
+++ 08/code/baum_analyse.m
@@ -63,7 +63,7 @@ for ya=1:Ly
 for xa=1:Lx
    if bond_feld(xa,ya) <= 0
        cluster=cluster+1; % neuer Index vergeben
-       bond_feld(xa,ya) = cluster;
+%         bond_feld(xa,ya) = cluster;
        bond_liste(1,:)=[xa ya];
        n=1; % letzter benutzter Listenplatz
        i=0;
@@ -72,7 +72,8 @@ for xa=1:Lx
            x=bond_liste(i,1);y=bond_liste(i,2);
            % rechten Nachbarn pruefen
            if x < Lx,
-               if bond_feld(x+1,y) == -2 | bond_feld(x+1,y) == -3
+%  	      bond_feld(x,y)
+               if ((bond_feld(x,y) == -2 || bond_feld(x,y) == -3) && bond_feld(x+1,y) <= 0)
                    bond_feld(x+1,y)=cluster;
                    n=n+1;
                    bond_liste(n,:)=[x+1 y];
@@ -80,7 +81,7 @@ for xa=1:Lx
            end
            % linken Nachbarn pruefen
            if x > 1
-               if bond_feld(x-1,y) == -3 | bond_feld(x-1,y) == -3
+               if ((bond_feld(x-1,y) == -2 || bond_feld(x-1,y) == -3) && bond_feld(x,y) <= 0)
                    bond_feld(x-1,y)=cluster;
                    n=n+1;
                    bond_liste(n,:)=[x-1 y];
@@ -88,7 +89,7 @@ for xa=1:Lx
            end
            % oberen Nachbarn pruefen
            if y < Ly
-               if bond_feld(x,y+1) == -1 | bond_feld(x,y+1) == -3
+               if ((bond_feld(x,y) == -1 || bond_feld(x,y) == -3) && bond_feld(x,y+1) <= 0)
                    bond_feld(x,y+1)=cluster;
                    n=n+1;
                    bond_liste(n,:)=[x y+1];
@@ -96,7 +97,7 @@ for xa=1:Lx
            end
            % unteren Nachbarn pruefen
            if y > 1
-               if bond_feld(x,y-1) == -1 | bond_feld(x,y-1) == -3
+               if ((bond_feld(x,y-1) == -1 || bond_feld(x,y-1) == -3) && bond_feld(x,y) <= 0)
                    bond_feld(x,y-1)=cluster;
                    n=n+1;
                    bond_liste(n,:)=[x y-1];
@@ -104,6 +105,7 @@ for xa=1:Lx
            end
        end % while loop: Cluster komplett
 %       fprintf('\n Cluster % i aus %i  Punkten \n',cluster,n)
+       bond_feld(xa,ya) = cluster;
    end
 end
 end
