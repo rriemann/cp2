7769bf95f7e4f990fbb895d9c6445ae5a51a0994
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2011-01-10 01:11:16 +0100

    fix + plot in bond_perk.m

--- 08/code/bond_perk.m
+++ 08/code/bond_perk.m
@@ -10,8 +10,8 @@ feld_rechts = (rand(L,L-1) > p); % 1 = verbindung nach rechts
 feld_oben = (rand(L-1,L) > p); % 1 = verbindung nach links
 feld = zeros(L,L);
 for i=1:L-1
-  feld(i,:) -= 2*feld_rechts;
-  feld(:,i) -= feld_oben;
+  feld(:,i) -= 2*feld_rechts(:,i);
+  feld(i,:) -= feld_oben(i,:);
 end
 flipud(feld.')  % druck-plot gegen (x,y)
 % Bedeutung feld-Werte: -1=leer, 0=besetzt, andere:
@@ -25,27 +25,20 @@ for y=1:L
 end
 % Linien
 for x=1:L-1, for y=1:L
-    if feld(x,y) >= 0 & feld(x+1,y) >= 0, plot([x x+1],[y y],'-r'); end
-end,end
-for x=1:L, for y=1:L-1
-    if feld(x,y) >= 0 & feld(x,y+1) >= 0, plot([x x],[y y+1],'-r'); end
+    if rem(feld(x,y),2) == -1 % verbindung nach oben
+      plot([x x],[y y+1],'-b');
+    end
+    if feld(x,y) < -1 % verbindung nach rechts
+      plot([x x+1],[y y],'-b');
+    end
 end,end
 axis off;
 
 pause
-
-disp('Ergebnis, Baumsuche:');
-[feld] = baum_analyse(feld);
-flipud(feld.')
-disp('Ergebnis, Hoshen-Kopelman:');
-[feld] = hoshen_kopelman(feld);
-flipud(feld.')
-
-%  bondperkolation - alle nachbarn immer verbunden, aber nur
-%  einige der verbindungen sind aktiv
-
-%  dann immer fallunterscheidung zwischen diesen faellen
-%  die matrix hat demnach auch nur (Lx-1)x(Ly-1) als dimension
-%  von jedem punkt aus werden nur die rechten bzw. unteren nachbarn ueberprueft
-%  hab mal versucht, den code in baum_analyse ab zeile 58 so aufzubauen; der aufruf
-%  in dieser datei hier fehlt noch; und in der anderen datei wird noch einiges falsch sein ;)
+%
+%  disp('Ergebnis, Baumsuche:');
+%  [feld] = baum_analyse(feld);
+%  flipud(feld.')
+%  disp('Ergebnis, Hoshen-Kopelman:');
+%  [feld] = hoshen_kopelman(feld);
+%  flipud(feld.')
