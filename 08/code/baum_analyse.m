1d6c4c160e072c634e3d309b44d5e0fb1a7a3cf0
Thomas Murach <asiasuppenesser@gmx.de>
2011-01-10 12:09:19 +0100

    ana zeros

--- 08/code/baum_analyse.m
+++ 08/code/baum_analyse.m
@@ -1,60 +1,8 @@
 function [bond_feldr] = baum_analyse(bond_feld)
 % kate: remove-trailing-space on; replace-trailing-space-save on; indent-width 2; indent-mode normal; syntax matlab; space-indent on;
 % Clusteranalyse mit Baumsuche
-bond_feldr=bond_feld;
+bond_feldr = zeros(size(bond_feld));
 [Lx,Ly]=size(bond_feld);
-
-%  liste=zeros(Lx*Ly,2); % Arbeitsliste fuer Clustersuche
-%  cluster=0;
-%
-%  for ya=1:Ly
-%  for xa=1:Lx
-%     if feldr(xa,ya)==0
-%         cluster=cluster+1; % neuer Index vergeben
-%         feldr(xa,ya)=cluster;
-%         liste(1,:)=[xa ya];
-%         n=1; % letzter benutzter Listenplatz
-%         i=0;
-%         while i < n
-%             i=i+1;
-%             x=liste(i,1);y=liste(i,2);
-%             % rechten Nachbarn pruefen
-%             if x < Lx,
-%                 if feldr(x+1,y)==0
-%                     feldr(x+1,y)=cluster;
-%                     n=n+1;
-%                     liste(n,:)=[x+1 y];
-%                 end
-%             end
-%             % linken Nachbarn pruefen
-%             if x > 1
-%                 if feldr(x-1,y)==0
-%                     feldr(x-1,y)=cluster;
-%                     n=n+1;
-%                     liste(n,:)=[x-1 y];
-%                 end
-%             end
-%             % oberen Nachbarn pruefen
-%             if y < Ly
-%                 if feldr(x,y+1)==0
-%                     feldr(x,y+1)=cluster;
-%                     n=n+1;
-%                     liste(n,:)=[x y+1];
-%                 end
-%             end
-%             % unteren Nachbarn pruefen
-%             if y > 1
-%                 if feldr(x,y-1)==0
-%                     feldr(x,y-1)=cluster;
-%                     n=n+1;
-%                     liste(n,:)=[x y-1];
-%                 end
-%             end
-%         end % while loop: Cluster komplett
-%  %       fprintf('\n Cluster % i aus %i  Punkten \n',cluster,n)
-%     end
-%  end
-%  end
 
 %  bondperkolation
 bond_liste = zeros(Lx*Ly,2);
@@ -82,7 +30,7 @@ for xa=1:Lx
            end
            % linken Nachbarn pruefen
             if x > 1
-                if (bond_feld(x-1,y) == -2 || bond_feld(x-1,y) == -3)
+                if ((bond_feld(x-1,y) == -2 || bond_feld(x-1,y) == -3) && bond_feldr(x-1,y) <= 0)
                     bond_feldr(x-1,y)=cluster;
                     n=n+1;
                     bond_liste(n,:)=[x-1 y];
@@ -98,7 +46,7 @@ for xa=1:Lx
            end
             % unteren Nachbarn pruefen
             if y > 1
-                if (bond_feld(x,y-1) == -1 || bond_feld(x,y-1) == -3)
+                if ((bond_feld(x,y-1) == -1 || bond_feld(x,y-1) == -3) && bond_feldr(x,y-1) <=0)
                     bond_feldr(x,y-1)=cluster;
                     n=n+1;
                     bond_liste(n,:)=[x y-1];
