8884da818ddd93ccf414403a13f92b0691f353c8
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2011-01-17 16:15:50 +0100

    reformatted

--- 09/code/R250.c
+++ 09/code/R250.c
@@ -1,6 +1,6 @@
 /* R250.c
-   R250 Shift-Register Zufallszahlen Generator 
-  
+   R250 Shift-Register Zufallszahlen Generator
+
    Bevor Zufallszahlen erzeugt werden, muss einmalig die Initialisierung
    mit initR250 durchgefuehrt werden. Sonst ist der Rueckgabewert negativ. */
 
@@ -11,117 +11,120 @@
 
 static unsigned int I[250];
 static int i1;
-static double maxINV=-1.0;
+static double maxINV = -1.0;
 
 /*****************************************************************************/
 /* void initR250
    Initialisierung des R250 Shift-Register Zufallszahlengenerators. Hierzu
-   wird zunaechst rand() mittels srand(seed) initialisiert. Da rand() 
-   Integerzahlen liefert, werden diese durch RAND_MAX dividiert und 
+   wird zunaechst rand() mittels srand(seed) initialisiert. Da rand()
+   Integerzahlen liefert, werden diese durch RAND_MAX dividiert und
    anschliessend mit dem maximalen Integer multipliziert.
    Diese Integerzahlen initialisieren das globale Array I, welches nach-
    folgend noch mit zwei warm-up Runden des R250 durchmischt wird.
-   Der Positionsindex i1 (auf  ein Element von I) wird auf 0 gesetzt 
+   Der Positionsindex i1 (auf  ein Element von I) wird auf 0 gesetzt
    und der Wert des inversen maximalen Integers (maxINV) ermittelt. */
 
-extern void initR250(int seed) { 
-unsigned int m;
-int k; 
-double d;
-               /* maximalen Integer erzeugen und invertieren */
-  m = ~0;           
-  maxINV = 1.0 /((double) m);  
+extern void initR250(int seed)
+{
+    unsigned int m;
+    int k;
+    double d;
+    /* maximalen Integer erzeugen und invertieren */
+    m = ~0;
+    maxINV = 1.0 / ((double) m);
 
-                     /* Initialisierung mit einfachen rand() */
-  srand(seed);
-  for (k=0; k<250; k++)
-    I[k]=(unsigned int)((rand() / (double) RAND_MAX)*m);
+    /* Initialisierung mit einfachen rand() */
+    srand(seed);
+    for(k = 0; k < 250; k++)
+        I[k] = (unsigned int)((rand() / (double) RAND_MAX) * m);
 
-  i1 = 0;                         /* setzen des Startvektors */
+    i1 = 0;                         /* setzen des Startvektors */
 
-                     /* zwei weitere warm-up Runden mit R250 */
-  for (k=0; k<500; k++)
-    d = R250(); 
+    /* zwei weitere warm-up Runden mit R250 */
+    for(k = 0; k < 500; k++)
+        d = R250();
 }
 
 
 /*****************************************************************************/
-/* int saveR250 
+/* int saveR250
    Speichert Zustand des R250 Generators als binary file, der Dateiname wird
-   als Argument gegeben. Rueckgabewert ist 0 bei fehlerfreier IO und 1 bei 
+   als Argument gegeben. Rueckgabewert ist 0 bei fehlerfreier IO und 1 bei
    einem Fehler. */
 
-extern int saveR250(char *fname) {
-FILE *out;
-  out=fopen(fname,"w");
-  if (out) {
-       fwrite(&i1, sizeof(int), 1, out);  
-       fwrite(&I[0], sizeof(int), 250, out);   
-       fclose(out);
-       return 0; 
-  }
-  else {
-       printf("ERROR WRITING R250 STATE TO FILE!\n"); 
-       return 1; 
-  }
+extern int saveR250(char *fname)
+{
+    FILE *out;
+    out = fopen(fname, "w");
+    if(out) {
+        fwrite(&i1, sizeof(int), 1, out);
+        fwrite(&I[0], sizeof(int), 250, out);
+        fclose(out);
+        return 0;
+    } else {
+        printf("ERROR WRITING R250 STATE TO FILE!\n");
+        return 1;
+    }
 }
 
 
 /*****************************************************************************/
-/* int loadR250 
+/* int loadR250
    Laedt Zustand des R250 Generators aus einem binary file. Der Dateiname wird
    als Argument gegeben. Rueckgabewert ist 0, bei fehlerfreier IO, 1 bei Fehler
-   und 2 falls maxINV sich unterscheidet (als Indikator fuer unterschiedlich 
+   und 2 falls maxINV sich unterscheidet (als Indikator fuer unterschiedlich
    grosse int Datentypen. */
 
-extern int loadR250(char *fname) {
-FILE *in;
+extern int loadR250(char *fname)
+{
+    FILE *in;
 
-  in=fopen(fname,"r");
-  if (in) {
-       fread(&i1, sizeof(int), 1, in);  
-       fread(&I[0], sizeof(int), 250, in);   
-       fclose(in);
-       return 0; 
-  }
-  else {
-       printf("ERROR LOADING R250 STATE FROM FILE!\n"); 
-       return 1; 
-  }
+    in = fopen(fname, "r");
+    if(in) {
+        fread(&i1, sizeof(int), 1, in);
+        fread(&I[0], sizeof(int), 250, in);
+        fclose(in);
+        return 0;
+    } else {
+        printf("ERROR LOADING R250 STATE FROM FILE!\n");
+        return 1;
+    }
 }
 
 
 /*****************************************************************************/
-/* double R250(void) 
-   Implementierung des R250 Shift-Register Zufallszahlengenerators.  
+/* double R250(void)
+   Implementierung des R250 Shift-Register Zufallszahlengenerators.
    Rueckgabewert ist eine "Zufallszahl" in double. */
 
-extern double R250(void){ 
-int i2;
-double d;
-                 /* Zweiter Positionsindex i2 wird ermittelt */
-   i2 = (i1>=147) ? i1-147 : i1+103;
+extern double R250(void)
+{
+    int i2;
+    double d;
+    /* Zweiter Positionsindex i2 wird ermittelt */
+    i2 = (i1 >= 147) ? i1 - 147 : i1 + 103;
 
-   I[i1]^=I[i2];              /* BitXOR: I[i1]= I[i1]^I[i2]; */
+    I[i1] ^= I[i2];            /* BitXOR: I[i1]= I[i1]^I[i2]; */
 
-   d=(I[i1])*maxINV;                 /* create double number */
- 
-   i1 = (i1>=249) ? 0 : i1+1;       /* Index i1 weitersetzen */
+    d = (I[i1]) * maxINV;             /* create double number */
 
-  return (d);
+    i1 = (i1 >= 249) ? 0 : i1 + 1;   /* Index i1 weitersetzen */
+
+    return (d);
 }
 
 /*****************************************************************************/
 /* printI250(void)
    Ausgabe der Elemente des globalen Array I, i1 und maxINV. */
 
-extern int printI250(void){
-int k;
+extern int printI250(void)
+{
+    int k;
 
-  for (k=0; k<250; k++)
-     printf("%i \t %iu \n", k, I[k]);
- 
-  printf("i1 = %i \n", i1);
-  printf("maxINV = %e \n", maxINV); 
-  return 0;
+    for(k = 0; k < 250; k++)
+        printf("%i \t %iu \n", k, I[k]);
+
+    printf("i1 = %i \n", i1);
+    printf("maxINV = %e \n", maxINV);
+    return 0;
 }
--- 09/code/cluster_analyse.c
+++ 09/code/cluster_analyse.c
@@ -6,7 +6,8 @@
 
 /*      Cluster-Analyse a la Hoshen Kopelman */
 
-void hoshen_kopelman(int **feld, int L){
+void hoshen_kopelman(int **feld, int L)
+{
 
     int x, y ;                      /* aktueller Punkt, Feldindex = x+y*L */
     int neu;                        /* neue Clusternummer */
@@ -16,62 +17,58 @@ void hoshen_kopelman(int **feld, int L){
 
     int *label;
 
-    /* Arbeitslist fuer Clustersuche dynamisch alloziert: */ 
+    /* Arbeitslist fuer Clustersuche dynamisch alloziert: */
     /* vorlaeufige -> endgueltige Num. */
-    label = (int *) malloc((L*L+2)*sizeof(int));
+    label = (int *) malloc((L * L + 2) * sizeof(int));
 
-    LEER = L*L+1; 
+    LEER = L * L + 1;
     label[LEER] = LEER;
     neu = 1;
 
-    for (y = 0; y < L; y++) for (x = 0; x < L; x++) 
-    {
-	if (feld[y][x] == -1)         /* leerer Punkt */
-	{
-	    feld[y][x] = LEER;
-	} else {                         /* besetzter Punkt */
+    for(y = 0; y < L; y++) for(x = 0; x < L; x++) {
+            if(feld[y][x] == -1) {        /* leerer Punkt */
+                feld[y][x] = LEER;
+            } else {                         /* besetzter Punkt */
 
-	    /* linker Nachbar */
-	    if (x > 0) {
-		links = feld[y][x-1];
-	    } else {
-		links = LEER;          
-	    }
+                /* linker Nachbar */
+                if(x > 0) {
+                    links = feld[y][x-1];
+                } else {
+                    links = LEER;
+                }
 
 
-	    /* unterer Nachbar */
-	    if (y > 0) {
-		unten = feld[y-1][x];
-		while (label[unten] < unten) unten = label[unten];
-	    } else {
-		unten = LEER;         
-	    }
+                /* unterer Nachbar */
+                if(y > 0) {
+                    unten = feld[y-1][x];
+                    while(label[unten] < unten) unten = label[unten];
+                } else {
+                    unten = LEER;
+                }
 
-	    if (links == LEER  &&  unten == LEER) {
-		feld[y][x] = neu;
-		label[neu] = neu; neu++;   /* neue Nummer vergeben */
-	    }
-	    else if (links < unten) {
-		feld[y][x] = links;
-		if (unten < LEER) label[unten] = links;  /* Cluster verbinden */
-	    }
-	    else {
-		feld[y][x] = unten;
-		if (links < LEER) label[links] = unten; /* Cluster verbinden */
-	    }
-	} /* besetzter Punkt */
-    }       
+                if(links == LEER  &&  unten == LEER) {
+                    feld[y][x] = neu;
+                    label[neu] = neu; neu++;   /* neue Nummer vergeben */
+                } else if(links < unten) {
+                    feld[y][x] = links;
+                    if(unten < LEER) label[unten] = links;   /* Cluster verbinden */
+                } else {
+                    feld[y][x] = unten;
+                    if(links < LEER) label[links] = unten;  /* Cluster verbinden */
+                }
+            } /* besetzter Punkt */
+        }
 
     /* vorlaeufige Nummerierung fertig */
 
-    for (y = 0; y < L; y++) for (x = 0; x < L; x++) {
-	n = feld[y][x];
-	while (label[n] < n) n = label[n];  /* gutes Label finden */
-	if (n == LEER) {
-	    feld[y][x] = -1;     /* Leerstellen -> -1 */
-	} else {
-	    feld[y][x] = n;
-	}
-    }
+    for(y = 0; y < L; y++) for(x = 0; x < L; x++) {
+            n = feld[y][x];
+            while(label[n] < n) n = label[n];   /* gutes Label finden */
+            if(n == LEER) {
+                feld[y][x] = -1;     /* Leerstellen -> -1 */
+            } else {
+                feld[y][x] = n;
+            }
+        }
     free(label);
 }
