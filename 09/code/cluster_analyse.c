e904015c87f650ce8bd485bcced019c6120617bc
Thomas Murach <murach@physik.hu-berlin.de>
2011-01-17 02:15:07 +0100

    tex: code drin

--- 09/code/cluster_analyse.c
+++ 09/code/cluster_analyse.c
@@ -4,75 +4,6 @@
 #include <stdlib.h>
 #include "cluster_analyse.h"
 
-/*      Cluster-Analyse mit Baumsuche */
-
-void baum_analyse(int **feld, int L){
-
-    int xa, ya;                        /* Anfangspunkt eines Clusters */
-    int cluster;                       /* Cluster-Nummer */
-    int n;                             /* aktuelle Clustergroesse */
-    int i;                             /* Arbeitspunkt in den Listen */
-    int x, y;                          /* aktueller Punkt */
-
-    int *xliste,*yliste;
-
-    /* Arbeitslisten fuer Clustersuche dynamisch alloziert: */ 
-    xliste = (int *) malloc(L*L*sizeof(int));
-    yliste = (int *) malloc(L*L*sizeof(int));
-
-    cluster = 0;
-    for (ya = 0; ya < L; ya++) for (xa = 0; xa < L; xa++) 
-    {
-	if (feld[ya][xa] != 0) continue;
-	cluster ++;             /* naechstes Cluster anfangen */
-	feld[ya][xa] = cluster;
-	xliste[0] = xa; yliste[0] = ya;
-	n = 0; 
-	i = -1;
-
-	while (i < n) {
-	    i++; 
-	    x = xliste[i]; 
-	    y = yliste[i]; 
-
-	    if ((x < L-1) && (feld[y][x+1] == 0)) {     /* rechter Nachbar */
-		feld[y][x+1] = cluster;
-		n++; 
-		xliste[n] = x+1; 
-		yliste[n] = y;
-	    } 
-
-	    if ((x > 0) && (feld[y][x-1] == 0)) {       /* linker Nachbar */
-		feld[y][x-1] = cluster;
-		n++; 
-		xliste[n] = x-1; 
-		yliste[n] = y;
-	    } 
-
-	    if ((y < L-1) && (feld[y+1][x] == 0)) {     /* oberer Nachbar */
-		feld[y+1][x] = cluster;
-		n++; 
-		xliste[n] = x; 
-		yliste[n] = y+1;
-	    } 
-
-	    if ((y > 0) && (feld[y-1][x] == 0 )) {     /* unterer Nachbar */
-		feld[y-1][x] = cluster;
-		n++; 
-		xliste[n] = x; 
-		yliste[n] = y-1;
-	    }
-
-	}     /* while beendet: Cluster ist komplett */
-	/* printf("cluster # %i : Groesse = %i\n",cluster,n+1); */
-    }     /* xa &ya */
-
-    free(xliste);
-    free(yliste);
-
-}  /* baum_analyse */
-
-
 /*      Cluster-Analyse a la Hoshen Kopelman */
 
 void hoshen_kopelman(int **feld, int L){
@@ -143,4 +74,4 @@ void hoshen_kopelman(int **feld, int L){
 	}
     }
     free(label);
-}     /* hoshen_kopelman */
+}
