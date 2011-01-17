1056560cfdc045d0307c1ff944e193bf6b061ab2
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2011-01-17 12:16:52 +0100

    fix in S

--- 09/code/punkt_perk.c
+++ 09/code/punkt_perk.c
@@ -13,7 +13,7 @@ field malloc_field(int L);
 int perkolation(field feld, int L);
 double P(field feld, int L, int perk_cluster);
 int dsc_sorter(const void *i1, const void *i2);
-int* cluster_sizes(int *array, int size);
+int* cluster_sizes(int *array, int size, int perk_cluster);
 
 int main(void){
 
@@ -56,21 +56,17 @@ int main(void){
 //             perkolierender cluster?
             int perk_cluster = perkolation(feld, L);
 	    
-            if ( perk_cluster != -1) {
+            if ( perk_cluster != 0) {
                 P_inf_sum += P(feld, L, perk_cluster);
             }
 
-            int *ns = cluster_sizes(feld[0],L2);
+            int *ns = cluster_sizes(feld[0],L2,perk_cluster);
             int s2 = 0;
             int s1 = 0;
             for(int i = 0; i < L2; ++i) {
                 if (ns[i] == 0) break;
                 s2 += ns[i]*ns[i];
                 s1 += ns[i];
-            }
-            if (perk_cluster != -1) {
-              s2 -= perk_cluster*perk_cluster;
-              s1 -= perk_cluster;
             }
             S_sum += (double) s1/s2;
         }
@@ -106,7 +102,7 @@ void print_field(field feld, int L)
 }
 
 int perkolation(field feld, int L){
-    int j, k, clusternummer = -1;
+    int j, k, clusternummer = 0;
     for (j = 0; j < L; j++) {
 	for (k = 0; k < L; k++) {
 	    if ( feld[j][0] == feld[k][L-1] && feld[j][0] != -1 ) {
@@ -143,24 +139,25 @@ int dsc_sorter(const void *i1, const void *i2) {
     return *(int *)i2 - *(int *)i1; /* absteigend sortiert */
 }
 
-int* cluster_sizes(int *array, int size) {
+int* cluster_sizes(int *array, int size, int perk_cluster) {
     int *n;
     n = malloc(size*sizeof(int));
     for (int i = 0; i < size; ++i) {
         n[i] = 0;
     }
     qsort(array, size, sizeof(array), dsc_sorter);
-    int current_cluster = array[0];
-    int j = 1;
-    n[j] = 1;
-    for (int i = 2; i < size; ++i) {
+    int current_cluster = 0;
+    int j = -1;
+    for (int i = 0; i < size; ++i) {
         if (array[i] == -1) {
             break;
-        } else if (array[i] != current_cluster) {
+        } else if (array[i] == current_cluster) {
+            n[j] += 1;
+        } else if (array[i] != perk_cluster) {
             ++j;
             current_cluster = array[i];
+            n[j] += 1;
         }
-        n[j] += 1;
     }
     qsort(n, size, sizeof(int), dsc_sorter);
     return n;
