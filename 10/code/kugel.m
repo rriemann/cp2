b3379af96d29f7a310153b682a1e6e853870616c
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2011-01-23 19:39:05 +0100

    performance improvements

--- 10/code/kugel.m
+++ 10/code/kugel.m
@@ -1,21 +1,24 @@
 %  Berechnung des Kugelvolumens
 
 R = 2;
+R2 = R^2;
 a = R;			% a = 1;
+a2 = a^2;
 N = 1e4;
 M = 5;
 sum_f = 0;
 
-x = R*rand(1,N);	% nur positive achsen
-y = R*rand(1,N);
-z = R*rand(1,N);
+x2 = R2*rand(1,N).^2;	% nur positive achsen
+y2 = R2*rand(1,N).^2;
+z2 = R2*rand(1,N).^2;
 
 for i=[1:N]
-  if x(i) <= a && x(i)*x(i) + y(i)*y(i) + z(i)*z(i) <= R*R
+  if x(i) <= a && x2(i) + y2(i) + z2(i) <= R2
     sum_f += 1;
   end
 end
 
 sum_f
 
-V = sum_f/N*R^2*a*8
+V = sum_f/N*R2*a*8
+% kate: remove-trailing-space on; replace-trailing-space-save on; indent-width 2; indent-mode normal; syntax matlab; space-indent on;
